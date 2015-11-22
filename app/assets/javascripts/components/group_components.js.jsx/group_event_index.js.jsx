b0913d0ac33fc3eea1743bd04f1b471953e9bed3
Cody Pizzaia <cpizzaia@asu.edu>
2015-11-21 21:02:58 -0500

    limit user list on event

--- app/assets/javascripts/components/group_components.js.jsx/group_event_index.js.jsx
+++ app/assets/javascripts/components/group_components.js.jsx/group_event_index.js.jsx
@@ -5,7 +5,7 @@ var GroupEventIndex = React.createClass({
         <h2 className="group-event-index-title">{this.props.event.title}</h2>
         <h3 className="group-event-index-start-time">{this.props.event.start_date}</h3>
         <h3 className="group-event-index-address">{this.props.event.address}</h3>
-        <GroupEventUserList users={this.props.event.users}/>
+        <GroupEventUserList users={this.props.event.users.slice(0,10)}/>
         <h4 className="group-event-index-members">{"Members going: " + this.props.event.users.length}</h4>
         <article className="group-event-index-description">{this.props.event.description}</article>
       </div>
