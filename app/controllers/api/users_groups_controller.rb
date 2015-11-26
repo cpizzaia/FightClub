135cbe86159f29812ab0f706c5ab5ea660e09f8f
Cody Pizzaia <cpizzaia@asu.edu>
2015-11-26 10:07:46 -0500

    organizer cannot leave group

--- app/controllers/api/users_groups_controller.rb
+++ app/controllers/api/users_groups_controller.rb
@@ -11,16 +11,22 @@ class Api::UsersGroupsController < ApplicationController
   end
 
   def destroy
-    @users_group = UsersGroup.where({group_id: params[:id], user_id: current_user.id}).first
-    @group = @users_group.group
-    @group.all_users_events(current_user.id).destroy_all
-    @users_group.destroy
-    render "api/groups/show"
+    unless user_is_organizer?
+      @users_group = UsersGroup.where({group_id: params[:id], user_id: current_user.id}).first
+      @group = @users_group.group
+      @group.all_users_events(current_user.id).destroy_all
+      @users_group.destroy
+      render "api/groups/show"
+    end
   end
 
   private
   def users_group_params
     params.require("users_group").permit(:user_id, :group_id)
+  end
+
+  def user_is_organizer?
+    Group.find([params[:id]]).first.organizer_id == current_user.id
   end
 
 end
