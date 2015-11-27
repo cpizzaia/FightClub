cae8c3e4f8188aa479e87f805230344e4c497cda
Cody Pizzaia <cpizzaia@asu.edu>
2015-11-27 12:17:39 -0500

    add description on homepage

--- app/assets/javascripts/components/landing_page.js.jsx
+++ app/assets/javascripts/components/landing_page.js.jsx
@@ -3,6 +3,7 @@ var LandingPage = React.createClass({
   render: function(){
     return (
       <div>
+        <img className="description" src={FightClub.description}/>
         <div className="welcome-banner"></div>
         <GroupIndex />
       </div>
