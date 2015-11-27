61d1280bcb13ba94fd29da620d0f1bee0e6a4b10
Cody Pizzaia <cpizzaia@asu.edu>
2015-11-27 14:26:57 -0500

    change seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -117,248 +117,258 @@ User.create({
   profile_img: File.new("#{Rails.root}/app/assets/images/profile_images/zangief.gif")
 })
 
-# 
-# Group.create({
-#   title: "Addicted to the Shoryuken",
-#   description: "Join if you like to dragon punch",
-#   organizer_id: User.first.id,
-#   group_img: File.new("#{Rails.root}/app/assets/images/shoryuken.png"),
-#   member_noun: "Shotos",
-#   city: "New York",
-#   state: "NY"
-# })
-#
-# Group.create({
-#   title: "Test Group 2",
-#   description: "who cares",
-#   organizer_id: User.first.id,
-#   city: "New York",
-#   state: "NY"
-# })
-#
-# Group.create({
-#   title: "Test Group 3",
-#   description: "who cares",
-#   organizer_id: User.first.id,
-#   city: "New York",
-#   state: "NY"
-# })
-#
-# Group.create({
-#   title: "Test Group 4",
-#   description: "who cares",
-#   organizer_id: User.first.id,
-#   city: "New York",
-#   state: "NY"
-# })
 
 Group.create({
   title: "Airfield Addicts",
   description: "Join if you like to dragon punch",
-  organizer_id: User.first.id,
+  organizer_id: User.all[0].id,
   group_img: File.new("#{Rails.root}/app/assets/images/stages/airfield.png"),
   member_noun: "Addicts",
   city: "New York",
   state: "NY"
 })
 
+UsersGroup.create({group_id: 1, user_id: 1})
+
 Group.create({
   title: "Alley Ambushers",
   description: "Join if you like to dragon punch",
-  organizer_id: User.first.id,
+  organizer_id: User.all[1].id,
   group_img: File.new("#{Rails.root}/app/assets/images/stages/alley.png"),
   member_noun: "Ambushers",
   city: "New York",
   state: "NY"
 })
 
+UsersGroup.create({group_id: 2, user_id: 2})
+
 Group.create({
   title: "Brewery Brawlers",
   description: "Join if you like to dragon punch",
-  organizer_id: User.first.id,
+  organizer_id: User.all[2].id,
   group_img: File.new("#{Rails.root}/app/assets/images/stages/brewery.png"),
   member_noun: "Brawlers",
   city: "New York",
   state: "NY"
 })
 
+UsersGroup.create({group_id: 3, user_id: 3})
+
 Group.create({
   title: "Bridge Busters",
   description: "Join if you like to dragon punch",
-  organizer_id: User.first.id,
+  organizer_id: User.all[3].id,
   group_img: File.new("#{Rails.root}/app/assets/images/stages/bridge.png"),
   member_noun: "Busters",
   city: "New York",
   state: "NY"
 })
 
+UsersGroup.create({group_id: 4, user_id: 4})
+
 Group.create({
   title: "Construction Crazed",
   description: "Join if you like to dragon punch",
-  organizer_id: User.first.id,
+  organizer_id: User.all[4].id,
   group_img: File.new("#{Rails.root}/app/assets/images/stages/constructionsite.png"),
   member_noun: "Crazies",
   city: "New York",
   state: "NY"
 })
 
+UsersGroup.create({group_id: 5, user_id: 5})
+
 Group.create({
   title: "Diner Dashers",
   description: "Join if you like to dragon punch",
-  organizer_id: User.first.id,
+  organizer_id: User.all[5].id,
   group_img: File.new("#{Rails.root}/app/assets/images/stages/diner.png"),
   member_noun: "Dashers",
   city: "New York",
   state: "NY"
 })
 
+UsersGroup.create({group_id: 6, user_id: 6})
+
 Group.create({
   title: "Jungle Jammers",
   description: "Join if you like to dragon punch",
-  organizer_id: User.first.id,
+  organizer_id: User.all[6].id,
   group_img: File.new("#{Rails.root}/app/assets/images/stages/jungle.png"),
   member_noun: "Jammers",
   city: "New York",
   state: "NY"
 })
 
+UsersGroup.create({group_id: 7, user_id: 7})
+
 Group.create({
   title: "Lab Lungers",
   description: "Join if you like to dragon punch",
-  organizer_id: User.first.id,
+  organizer_id: User.all[7].id,
   group_img: File.new("#{Rails.root}/app/assets/images/stages/lab.png"),
   member_noun: "Lungers",
   city: "New York",
   state: "NY"
 })
 
+UsersGroup.create({group_id: 8, user_id: 8})
+
 Group.create({
   title: "Olympic Warriors",
   description: "Join if you like to dragon punch",
-  organizer_id: User.first.id,
+  organizer_id: User.all[8].id,
   group_img: File.new("#{Rails.root}/app/assets/images/stages/olympics.png"),
   member_noun: "Warriors",
   city: "New York",
   state: "NY"
 })
 
+UsersGroup.create({group_id: 9, user_id: 9})
+
 Group.create({
   title: "Plains Punchers",
   description: "Join if you like to dragon punch",
-  organizer_id: User.first.id,
+  organizer_id: User.all[9].id,
   group_img: File.new("#{Rails.root}/app/assets/images/stages/plains.png"),
   member_noun: "Punchers",
   city: "New York",
   state: "NY"
 })
 
+UsersGroup.create({group_id: 10, user_id: 10})
+
 Group.create({
   title: "Railyard Rumblers",
   description: "Join if you like to dragon punch",
-  organizer_id: User.first.id,
+  organizer_id: User.all[10].id,
   group_img: File.new("#{Rails.root}/app/assets/images/stages/railyard.png"),
   member_noun: "Rumblers",
   city: "New York",
   state: "NY"
 })
 
+UsersGroup.create({group_id: 11, user_id: 11})
+
 Group.create({
   title: "Rural Rampants",
   description: "Join if you like to dragon punch",
-  organizer_id: User.first.id,
+  organizer_id: User.all[11].id,
   group_img: File.new("#{Rails.root}/app/assets/images/stages/rural.png"),
   member_noun: "Rampants",
   city: "New York",
   state: "NY"
 })
 
+UsersGroup.create({group_id: 12, user_id: 12})
+
 Group.create({
   title: "Ship Shooters",
   description: "Join if you like to dragon punch",
-  organizer_id: User.first.id,
+  organizer_id: User.all[12].id,
   group_img: File.new("#{Rails.root}/app/assets/images/stages/ship.png"),
   member_noun: "Shooters",
   city: "New York",
   state: "NY"
 })
 
+UsersGroup.create({group_id: 13, user_id: 13})
+
 Group.create({
   title: "Temple Tempests",
   description: "Join if you like to dragon punch",
-  organizer_id: User.first.id,
+  organizer_id: User.all[13].id,
   group_img: File.new("#{Rails.root}/app/assets/images/stages/temple.png"),
   member_noun: "Tempests",
   city: "New York",
   state: "NY"
 })
 
+UsersGroup.create({group_id: 14, user_id: 14})
+
 Group.create({
   title: "Town Tumblers",
   description: "Join if you like to dragon punch",
-  organizer_id: User.first.id,
+  organizer_id: User.all[14].id,
   group_img: File.new("#{Rails.root}/app/assets/images/stages/town.png"),
   member_noun: "Tumblers",
   city: "New York",
   state: "NY"
 })
 
+UsersGroup.create({group_id: 15, user_id: 15})
+
 Group.create({
   title: "Volcano Valiants",
   description: "Join if you like to dragon punch",
-  organizer_id: User.first.id,
+  organizer_id: User.all[15].id,
   group_img: File.new("#{Rails.root}/app/assets/images/stages/volcano.png"),
   member_noun: "Valiants",
   city: "New York",
   state: "NY"
 })
 
+UsersGroup.create({group_id: 16, user_id: 16})
 
-UsersGroup.create({group_id: 2, user_id: User.first.id})
-UsersGroup.create({group_id: 3, user_id: User.first.id})
-UsersGroup.create({group_id: 2, user_id: User.all[1].id})
-UsersGroup.create({group_id: 4, user_id: User.all[1].id})
 
 User.all.each do |user|
   UsersGroup.create({group_id: 1, user_id: user.id})
 end
 
-Event.create({
-  start_time: DateTime.new(2016,1,1),
-  title: "Fight Festival",
-  description: "Fight all things!",
-  group_id: Group.first.id,
-  address: "Times Square"
-})
+def group_join_random_5_users (group)
+  users = []
+  while users.length <= 5
+    user = User.all.sample
+    if !users.include?(user) && group.organizer_id != user.id
+      UsersGroup.create({group_id: group.id, user_id: user.id})
+      users << user
+    end
+  end
+end
 
-Event.create({
-  start_time: DateTime.new(2016,2,1),
-  title: "Punchout Panic",
-  description: "Fight all things!",
-  group_id: Group.first.id,
-  address: "Hawaii"
-})
+Group.all.length.times do |i|
+  next if i == 0
+  group_join_random_5_users(Group.all[i])
+end
 
-Event.create({
-  start_time: DateTime.new(2016,3,1),
-  title: "Haymaker Harvest",
-  description: "Fight all things!",
-  group_id: Group.first.id,
-  address: "Great Wall of China"
-})
+Group.all.length.times do |i|
+  event = Event.create({
+    start_time: DateTime.new(2016,1,1),
+    title: "Fight Festival",
+    description: "Fight all things!",
+    group_id: Group.all[i].id,
+    address: "Times Square"
+  })
 
-Event.create({
-  start_time: DateTime.new(2015,9,1),
-  title: "Fall Fight",
-  description: "Fight all things!",
-  group_id: Group.first.id,
-  address: "Great Wall of China"
-})
+  UsersEvent.create({event_id: event.id, user_id: Group.all[i].organizer_id})
 
+  event = Event.create({
+    start_time: DateTime.new(2016,2,1),
+    title: "Punchout Panic",
+    description: "Fight all things!",
+    group_id: Group.all[i].id,
+    address: "Hawaii"
+  })
 
+  UsersEvent.create({event_id: event.id, user_id: Group.all[i].organizer_id})
 
-UsersEvent.create({event_id: 1, user_id: User.first.id})
-UsersEvent.create({event_id: 2, user_id: User.first.id})
-UsersEvent.create({event_id: 3, user_id: User.first.id})
-UsersEvent.create({event_id: 4, user_id: User.first.id})
-UsersEvent.create({event_id: 1, user_id: User.all[1].id})
-UsersEvent.create({event_id: 3, user_id: User.all[1].id})
+  event = Event.create({
+    start_time: DateTime.new(2016,3,1),
+    title: "Haymaker Harvest",
+    description: "Fight all things!",
+    group_id: Group.all[i].id,
+    address: "Great Wall of China"
+  })
+
+  UsersEvent.create({event_id: event.id, user_id: Group.all[i].organizer_id})
+
+  event = Event.create({
+    start_time: DateTime.new(2015,9,1),
+    title: "Fall Fight",
+    description: "Fight all things!",
+    group_id: Group.all[i].id,
+    address: "Pyramids of Giza"
+  })
+
+  UsersEvent.create({event_id: event.id, user_id: Group.all[i].organizer_id})
+end
