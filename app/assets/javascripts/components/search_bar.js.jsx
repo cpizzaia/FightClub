21c737855318430490d21cb8cb71756a8b99dcbb
Cody Pizzaia <cpizzaia@asu.edu>
2015-11-30 19:27:24 -0500

    change search bar to dynamically update

--- app/assets/javascripts/components/search_bar.js.jsx
+++ app/assets/javascripts/components/search_bar.js.jsx
@@ -6,21 +6,25 @@ var SearchBar = React.createClass({
     return({query: ""});
   },
 
-  _handleSubmit: function(e){
+  _handleChange: function(e){
     e.preventDefault();
-    if (this.state.query === ""){
+    if (e.currentTarget.value === ""){
       GroupStore.clearGroups();
       GroupApiUtil.fetchGroupsByPage(1);
     } else {
-      SearchApiUtil.search(this.state.query);
+      SearchApiUtil.search(e.currentTarget.value);
     }
   },
 
   render: function(){
     return (
-      <form className="search-bar group" onSubmit={this._handleSubmit}>
-        <input valueLink={this.linkState("query")} type="text" className="search-bar-input" placeholder="Find Your Fight"/>
-        <span className="search-bar-icon" onClick={this._handleSubmit}></span>
+      <form className="search-bar group">
+
+        <input type="text" className="search-bar-input"
+          placeholder="Find Your Fight"
+          onChange={this._handleChange}/>
+
+        <span className="search-bar-icon"></span>
       </form>
     );
   }
