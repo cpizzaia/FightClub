70cd38b0a458f1025487aee25d7df6062b9ce837
Cody Pizzaia <cpizzaia@asu.edu>
2015-12-17 09:35:13 -0500

    groups where not organizer test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe User, type: :model do
 
   before(:all) do
     @user = create(:user)
+    @group = Group.new(attributes_for(:group))
+    @group.organizer_id = @user.id
+    @group.save
   end
 
   describe "self.find_by_credentials" do
@@ -15,15 +18,21 @@ RSpec.describe User, type: :model do
 
   describe "groups_led" do
     it "returns the groups where the user is the organizer" do
-      @group = Group.new(attributes_for(:group))
-      @group.organizer_id = @user.id
-      @group.save
-      expect(@user.groups_led.first).to eq(@group)
+      expect(@user.groups_led).to include(@group)
     end
 
     it "user should have many groups_led" do
       t = User.reflect_on_association(:groups_led)
       expect(t.macro).to eq(:has_many)
+    end
+  end
+
+  describe "groups_where_not_organizer" do
+    it "returns the groups the user is not the organizer" do
+      @user2 = create(:user)
+      @group2 = @user2.groups_led.create(attributes_for(:group))
+      expect(@user2.groups_led).not_to include(@group)
+      expect(@user2.groups_led).to include(@group2)
     end
   end
 end
