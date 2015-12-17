a11c29b6ccb20e00a3dc233071d0525c56003b49
Cody Pizzaia <cpizzaia@asu.edu>
2015-12-17 10:02:24 -0500

    refactor user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,11 @@ require "rails_helper.rb"
 
 RSpec.describe User, type: :model do
 
-  before(:all) do
-    @user = create(:user)
-    @group = Group.new(attributes_for(:group))
-    @group.organizer_id = @user.id
-    @group.save
-  end
-
   describe ".find_by_credentials" do
+    before(:each) do
+      @user = create(:user)
+    end
+
     it "returns the correct user given an email and password" do
       result = User.find_by_credentials(@user.useremail, @user.password)
       expect(result).to eq(@user)
@@ -17,6 +14,13 @@ RSpec.describe User, type: :model do
   end
 
   describe "#groups_led" do
+    before(:each) do
+      @user = create(:user)
+      @group = Group.new(attributes_for(:group))
+      @group.organizer_id = @user.id
+      @group.save
+    end
+
     it "should have many groups_led" do
       t = User.reflect_on_association(:groups_led)
       expect(t.macro).to eq(:has_many)
@@ -28,15 +32,27 @@ RSpec.describe User, type: :model do
   end
 
   describe "groups_where_not_organizer" do
-    it "returns the groups the user is not the organizer" do
+    before(:each) do
+      @user = create(:user)
+      @group = @user.groups_led.create(attributes_for(:group))
       @user2 = create(:user)
       @group2 = @user2.groups_led.create(attributes_for(:group))
+    end
+
+    it "returns the groups the user is not the organizer" do
       expect(@user2.groups_led).not_to include(@group)
       expect(@user2.groups_led).to include(@group2)
     end
   end
 
   describe "#groups" do
+    before(:each) do
+      @user = create(:user)
+      @group = @user.groups_led.create(attributes_for(:group))
+      @user2 = create(:user)
+      @group2 = @user2.groups_led.create(attributes_for(:group))
+    end
+
     it "user should have many groups" do
       t = User.reflect_on_association(:groups)
       expect(t.macro).to eq(:has_many)
@@ -47,8 +63,6 @@ RSpec.describe User, type: :model do
     end
 
     it "does not return groups the user is not a part of" do
-      @user2 = create(:user)
-      @group2 = @user2.groups_led.create(attributes_for(:group))
       expect(@user.groups).not_to include(@group2)
     end
   end
